adc24996c93d7e5e04157eaa0288eafd1735132e
John Albright <John@johnalbightsMBP.attlocal.net>
2022-01-03 03:28:37 -0600

    Connected the Adjustment Block to the Timer components; todo: connect the reset button to the adjustment block components; idea: add context or redux to manage the state for all components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+// Global variables
+const initialSessionTime = 25;
+const initialBreakTime = 5;
 
 const AdjustmentBlock = props => {
 
@@ -8,31 +12,43 @@ const AdjustmentBlock = props => {
 
   const [number, setNumber] = useState(props.number);
 
+  const decrementCount = () => {
+    setNumber(number => number - 1 < 0 ? number : number - 1);
+  }
+
+  const incrementCount = () => {
+    setNumber(number => number + 1 > 60 ? number : number + 1);
+    //console.log("adjustment block:", number);
+    // props.changeTime(number);
+  }
+
+  useEffect(() => props.changeTime(number));
+
   let content = (
 
-    <div class="adjustment-block">
+    <div className="adjustment-block">
       <div 
-        class = "adjustment-label" 
+        className = "adjustment-label" 
         id = {id + "-label"}
-        >
+      >
         {id[0].toUpperCase() + id.slice(1)} Length
       </div>
       <div 
         id = {id + "-decrement"} 
-        onClick = {setNumber.bind(this, number - 1 < 0 ? number : number - 1)}
-        >
-          &#8722;
+        onClick = { decrementCount }
+      >
+        &#8722;
       </div>
       <div 
         id = {id + "-length"}
-        >
-          {number}
+      >
+        {number}
       </div>
       <div 
         id ={id + "-increment"} 
-        onClick = {setNumber.bind(this, number + 1)}
-        >
-          &#43;
+        onClick = { incrementCount }
+      >
+        &#43;
       </div>
     </div>
   );
@@ -42,12 +58,36 @@ const AdjustmentBlock = props => {
 
 const Timer = props => {
 
+  const [timeLeft, setTimeLeft] = useState(props.sessionTimeApp);
+
+  const [sessionTime, setSessionTime] = useState(props.sessionTimeApp);
+
+  const [breakTime, setBreakTime] = useState(props.breakTimeApp);
+
+  const reset = () => {
+    setTimeLeft(initialSessionTime);
+    setSessionTime(initialSessionTime);
+    setBreakTime(initialBreakTime);
+    props.resetBreak(initialBreakTime);
+    props.resetSession(initialSessionTime);
+  }
+
+  // const printTimeLeft = () => {
+  //   console.log(`time left: ${timeLeft}`);
+  // }
+
+  useEffect(() => {
+    setTimeLeft(props.sessionTimeApp);
+    setSessionTime(props.sessionTimeApp);
+    setBreakTime(props.breakTimeApp);
+  }, [props.sessionTimeApp, props.breakTimeApp]);
+
   let content = (
     <div id="timer-block">
       <div id="timer-label">Session</div>
-      <div id="time-left"></div>
+      <div id="time-left">{timeLeft}</div>
       <div id="start_stop">*</div>
-      <div id="reset">^</div>
+      <div id="reset" onClick = {reset}>^</div>
     </div>
   );
 
@@ -56,21 +96,66 @@ const Timer = props => {
 
 const App = props => {
 
+  const [breakTime, setBreakTime] = useState(initialBreakTime);
+  const [sessionTime, setSessionTime] = useState(initialSessionTime);
+  //const [reset, setReset] = useState(false);
+
+  const breakUpdate = (number) => {
+    setBreakTime(number);
+    //console.log(`break time: ${breakTime}`);
+  }
+
+  const sessionUpdate = (number) => {
+    setSessionTime(number);
+    //console.log(`session time: ${sessionTime}`);
+  }
+
+  /*
+  useEffect(() => {
+    if (breakTime === 5 && sessionTime === 25) {
+      resetBreakApp(5);
+      resetSessionApp()
+    }
+  });
+
+  const resetBreakApp = () => {
+
+  }
+
+  const resetSessionApp = () => {
+
+  }*/
+
+  /*
+  useEffect(() => {
+    sendBreakTimeToChild(breakTime);
+    sendSessionTimeToChild(sessionTime);
+  });
+
+  const sendBreakTimeToChild = (number) => number;
+  const sendSessionTimeToChild = (number) => number;*/
+
   let content = (
     <div id="main">
       <div id="app-title">25 + 5 Clock</div>
       <AdjustmentBlock 
         id = "break"
-        number = {5}
+        number = {initialBreakTime}
+        changeTime = {breakUpdate}
+        resetBreak = {breakUpdate}
       />
       <AdjustmentBlock 
         id = "session"
-        number = {25}
+        number = {initialSessionTime}
+        changeTime = {sessionUpdate}
+        resetSession = {sessionUpdate}
       />
       <Timer 
-
-      />
-      
+        sessionTimeApp = {sessionTime}
+        breakTimeApp = {breakTime}
+        //resetBreak = {breakUpdate}
+        //resetSession = {sessionUpdate}
+      /> 
     </div>
   );
 
